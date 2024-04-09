fac6d381b3a486b1eced3fd6463da267c5489eed
Alexei Eremin <draber06@gmail.com>
2024-04-09 22:42:20 +0300

    Alpha-bank - Add question 4

---
+++ old-interviews/alpha-bank/question4.js
@@ -0,0 +1,4 @@
+import assert from "assert";
+// Implement Array.prototype.some()
+// [2, 5, 8, 1, 4].some((element) => element > 10) -> false
+// [12, 5, 8, 1, 4].some((element) => element > 10) -> true
