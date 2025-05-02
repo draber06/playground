d63fcc77b0826090d82b4396022c9964387cdcb9
Alexei Eremin <draber06@gmail.com>
2025-05-02 08:51:18 +0300

    Add questions from tbank interview: t4

---
+++ old-interviews/tbank/t4/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+// Написать приложение с полем ввода для поиска героев
+// Поддерживает постраничную навигацию
+// Добавить дебаунс запросов
+function getPeople(name, page = 1, options = {}) {
+  return fetch(`https://rickandmortyapi.com/api/character?name=${name}&page=${page}`, options).then(
+    res => res.json()
+  );
+}
+
+function App() {
+  return `your code`;
+}
+
+export default App;
