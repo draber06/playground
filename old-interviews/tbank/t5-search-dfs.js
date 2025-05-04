527a8170c0fec741f2d722e778797a4ad55c460a
Alexei Eremin <draber06@gmail.com>
2025-05-04 08:21:10 +0300

    Add solution for tbank:t5

--- old-interviews/tbank/t5-search-dfs.js
+++ old-interviews/tbank/t5-search-dfs.js
@@ -33,7 +33,33 @@ const data = {
 };
 
 // TODO: Реализуйте эту функцию
-function search(path, object) {}
+function search(path, object) {
+    const keys = path.split(".");
+    let current = object;
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        if (key === "*") {
+            if (Array.isArray(current)) {
+                current = current.flatMap(c => search(keys.slice(i + 1).join("."), c));
+            } else if (typeof current === "object" && current !== null) {
+                current = Object.values(current);
+            } else {
+                current = undefined;
+            }
+
+            i++;
+        } else {
+            current = current[keys[i]];
+        }
+
+        if (current === null || current === undefined) {
+            break;
+        }
+    }
+
+    return current;
+}
 
 assert.deepEqual(search("albums.0.name", data), "AM");
 assert.deepEqual(search("artists.3.name", data), "Radiohead");
