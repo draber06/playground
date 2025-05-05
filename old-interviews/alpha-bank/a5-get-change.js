b79ddde055a90f14d8c4cad7b6ab2373d4eb9266
Alexei Eremin <draber06@gmail.com>
2025-05-05 08:06:14 +0300

    Add solution for alpha-bank:question5[part1]

--- old-interviews/alpha-bank/a5-get-change.js
+++ old-interviews/alpha-bank/a5-get-change.js
@@ -5,7 +5,27 @@ import assert from "node:assert/strict";
 
 // 2. На входе добавляется объект с текущим количеством купюр в банкомате
 
-function getMoney(amount, limits = {}) {}
+function getMoney(amount, limits = {}) {
+    const nominals = [5000, 1000, 500, 100, 50];
+    const change = nominals.reduce((acc, nominal) => ({ ...acc, [nominal]: 0 }), {});
+    let currentNominalPos = 0;
+    let currentAmount = amount;
+
+    while (currentAmount > 0 && currentNominalPos < nominals.length) {
+        const nominal = nominals[currentNominalPos];
+        const banknoteCount = Math.floor(currentAmount / nominal);
+        change[nominal] = banknoteCount;
+
+        currentAmount -= banknoteCount * nominal;
+        currentNominalPos++;
+    }
+
+    if (currentAmount) {
+        throw new Error("Нет сдачи");
+    }
+
+    return change;
+}
 
 assert.deepEqual(getMoney(6200), { 5000: 1, 1000: 1, 500: 0, 100: 2, 50: 0 });
 
