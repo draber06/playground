9b59fffc703b6398e02ab201c64030645d34897d
Alexei Eremin <draber06@gmail.com>
2025-05-05 07:31:00 +0300

    Add solution for alpha-bank:question4

--- old-interviews/alpha-bank/a4-array-some.js
+++ old-interviews/alpha-bank/a4-array-some.js
@@ -2,3 +2,22 @@ import assert from "assert";
 // Implement Array.prototype.some()
 // [2, 5, 8, 1, 4].some((element) => element > 10) -> false
 // [12, 5, 8, 1, 4].some((element) => element > 10) -> true
+
+Array.prototype.some = function (predicate, thisArg) {
+    for (let i = 0; i < this.length; i++) {
+        if (predicate.call(thisArg, this[i], i, this)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+assert.equal(
+    [2, 5, 8, 1, 4].some(element => element > 10),
+    false
+);
+assert.equal(
+    [12, 5, 8, 1, 4].some(element => element > 10),
+    true
+);
