0e64dfd9db87f6f3f944382da32b7e82ed1e6b4a
Alexei Eremin <draber06@gmail.com>
2025-05-05 09:28:25 +0300

    Udpate solution vk-messenger:t2

--- old-interviews/vk-messenger/vkm2-flat.js
+++ old-interviews/vk-messenger/vkm2-flat.js
@@ -1,7 +1,26 @@
 import assert from "assert";
 
-function flat(arr) {}
+function flat(arr) {
+    return arr.reduce(
+        (acc, current) => acc.concat(Array.isArray(current) ? flat(current) : current),
+        []
+    );
+}
 
-function iterativeFlat(arr) {}
+function iterativeFlat(arr) {
+    const stack = [...arr];
+    const result = [];
+
+    while (stack.length) {
+        const element = stack.pop();
+        if (Array.isArray(element)) {
+            stack.push(...element);
+        } else {
+            result.push(element);
+        }
+    }
+
+    return result.reverse();
+}
 
 assert.deepEqual(flat([1, 2, 3, [4, 5, [6, 7, 8, [9]]]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
