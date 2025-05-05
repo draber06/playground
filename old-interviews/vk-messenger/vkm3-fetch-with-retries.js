189853a4c887ff108fb7a7ef47d0a87bf94746eb
Alexei Eremin <draber06@gmail.com>
2025-05-05 09:58:30 +0300

    Add solution vk-messenger:t3

--- old-interviews/vk-messenger/vkm3-fetch-with-retries.js
+++ old-interviews/vk-messenger/vkm3-fetch-with-retries.js
@@ -1,1 +1,23 @@
-function fetchWithRetries(retriesCount = 0, ...fetchArgs) {}
+function fetchWithRetries(retriesCount = 0, ...fetchArgs) {
+    return fetch(...fetchArgs)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res;
+        })
+        .catch(() => {
+            const remainingRetries = retriesCount - 1;
+
+            if (!remainingRetries) {
+                throw new Error("Попытки закончились");
+            }
+            return fetchWithRetries(remainingRetries, ...fetchArgs);
+        });
+}
+
+fetchWithRetries(3, "https://rickandmortyapi.com/api/character/?name=asdfasdfasdf&status=alive")
+    .then(res => {
+        return res.json();
+    })
+    .then(res => console.log(res));
