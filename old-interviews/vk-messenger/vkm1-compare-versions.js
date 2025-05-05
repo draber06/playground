6eca9d101928a7214ac9edc49baf2fcf052dc501
Alexei Eremin <draber06@gmail.com>
2025-05-05 09:19:32 +0300

    Udpate solution vk-messenger:t1

--- old-interviews/vk-messenger/vkm1-compare-versions.js
+++ old-interviews/vk-messenger/vkm1-compare-versions.js
@@ -1,6 +1,23 @@
 import assert from "assert";
 
-function compareVersions(v1, v2) {}
+function compareVersions(v1, v2) {
+    if (v1 === v2) return 0;
+
+    const v1Segments = v1.split(".");
+    const v2Segments = v2.split(".");
+
+    while (v1Segments.length || v2Segments.length) {
+        const v1Segment = Number(v1Segments.shift() ?? 0);
+        const v2Segment = Number(v2Segments.shift() ?? 0);
+        if (v1Segment > v2Segment) {
+            return 1;
+        } else if (v1Segment < v2Segment) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
 
 assert.equal(compareVersions("2.0", "1.0"), 1);
 assert.equal(compareVersions("1.0", "2.0"), -1);
