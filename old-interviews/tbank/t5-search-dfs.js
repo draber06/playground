6afd4c54bbca6bd1f6ab607da714d003135f8d44
Alexei Eremin <draber06@gmail.com>
2025-05-12 16:19:40 +0300

    Update solution tbank:t5

--- old-interviews/tbank/t5-search-dfs.js
+++ old-interviews/tbank/t5-search-dfs.js
@@ -3,7 +3,7 @@ import assert from "assert";
 const data = {
     albums: [
         {
-            name: "AM",
+            name1: "AM",
             group: "Arctic Monkeys",
         },
         {
@@ -33,38 +33,38 @@ const data = {
 };
 
 // TODO: Реализуйте эту функцию
+// function search(path, object) {
+//     const keys = path.split(".");
+//     let current = object;
+
+//     for (let i = 0; i < keys.length; i++) {
+//         const key = keys[i];
+//         if (key === "*") {
+//             if (Array.isArray(current)) {
+//                 current = current.flatMap(c => search(keys.slice(i + 1).join("."), c));
+//             } else if (typeof current === "object" && current !== null) {
+//                 current = Object.values(current);
+//             } else {
+//                 current = undefined;
+//             }
+
+//             i++;
+//         } else {
+//             current = current[keys[i]];
+//         }
+
+//         if (current === null || current === undefined) {
+//             break;
+//         }
+//     }
+
+//     return current;
+// }
+
 function search(path, object) {
-    const keys = path.split(".");
-    let current = object;
-
-    for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        if (key === "*") {
-            if (Array.isArray(current)) {
-                current = current.flatMap(c => search(keys.slice(i + 1).join("."), c));
-            } else if (typeof current === "object" && current !== null) {
-                current = Object.values(current);
-            } else {
-                current = undefined;
-            }
-
-            i++;
-        } else {
-            current = current[keys[i]];
-        }
-
-        if (current === null || current === undefined) {
-            break;
-        }
-    }
-
-    return current;
-}
-
-function alternativeSearch(path, object) {
     const parts = path.split(".");
 
-    function resolvePath(paths, current) {
+    function resolvePath(parts, current) {
         if (parts.length === 0) return current;
 
         const [part, ...rest] = parts;
@@ -78,7 +78,7 @@ function alternativeSearch(path, object) {
             return [];
         }
 
-        if (current == null || !Object.hasOwn(current, part)) return undefined;
+        if (current == null || !(part in current)) return [];
 
         return resolvePath(rest, current[part]);
     }
