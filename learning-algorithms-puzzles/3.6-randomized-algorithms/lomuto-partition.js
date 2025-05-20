57d455708df84239305148018549e04e3022be1e
Alexei Eremin <draber06@gmail.com>
2025-05-20 15:12:25 +0300

    Implement lomuto partition

---
+++ learning-algorithms-puzzles/3.6-randomized-algorithms/lomuto-partition.js
@@ -0,0 +1,38 @@
+import assert from "node:assert";
+
+let inputData = "";
+
+process.stdin.setEncoding("utf-8");
+
+process.stdin.on("data", chunk => {
+    inputData += chunk;
+});
+
+process.stdin.on("end", () => {
+    const lines = inputData.trim().split("\n");
+    const seq = lines[1].trim().split(" ").map(Number);
+
+    console.log(...lomutoPartition(seq));
+});
+
+function lomutoPartition(array) {
+    const pivot = array[0];
+    let smallEnd = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        const current = array[i];
+        if (current >= pivot) continue;
+        smallEnd++;
+        for (let j = i; j > smallEnd; j--) {
+            array[j] = array[j - 1];
+        }
+        array[smallEnd] = current;
+    }
+
+    array[0] = array[smallEnd];
+    array[smallEnd] = pivot;
+
+    return array;
+}
+
+assert.deepEqual(lomutoPartition([4, 7, 2, 5, 3, 1, 8, 9, 6]), [1, 2, 3, 4, 7, 5, 8, 9, 6]);
