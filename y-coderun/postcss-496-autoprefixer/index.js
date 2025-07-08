a6b4bc56b6c520555b2c057327b4c72d25a0776c
Alexei Eremin <draber06@gmail.com>
2025-07-08 10:20:40 +0300

    Add 496-autoprefixer solution

--- y-coderun/postcss-496-autoprefixer/index.js
+++ y-coderun/postcss-496-autoprefixer/index.js
@@ -1,30 +1,42 @@
 /**
- * @type {import('postcss').PluginCreator}
+ * @type {import("postcss").PluginCreator}
  */
-module.exports = (opts = {}) => {
-    // Work with options here
-
+module.exports = (opts = { vendorPrefixes: {} }) => {
+    const { vendorPrefixes } = opts;
     return {
         postcssPlugin: "postcss-496-autoprefixer",
-        /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
-    */
+        Rule: rule => {
+            if (!rule.selector.includes("::")) return;
 
-        /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
+            rule.selector
+                .split(",")
+                .map(s => s.trim())
+                .forEach(selector => {
+                    const match = selector.match(/::\w+/);
+                    if (!match) return;
 
-        /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
-      }
-    }
-    */
+                    const pseudoElement = match[0];
+                    const prefixes = vendorPrefixes[pseudoElement];
+                    if (!prefixes) return;
+
+                    const baseSelector = selector.replace(pseudoElement, "");
+                    prefixes.forEach(prefix => {
+                        rule.cloneBefore({
+                            selector: `${baseSelector}::-${prefix}-${pseudoElement.slice(2)}`,
+                        });
+                    });
+                });
+        },
+        Declaration: {
+            "*": decl => {
+                const prefixes = vendorPrefixes[decl.prop];
+                if (!prefixes) return;
+
+                for (const prefix of prefixes) {
+                    decl.cloneBefore({ prop: `-${prefix}-` + decl.prop });
+                }
+            },
+        },
     };
 };
 
--- y-coderun/postcss-496-autoprefixer/index.test.js
+++ y-coderun/postcss-496-autoprefixer/index.test.js
@@ -6,13 +6,60 @@ const plugin = require("./");
 
 async function run(input, output, opts = {}) {
     let result = await postcss([plugin(opts)]).process(input, { from: undefined });
+
     equal(result.css, output);
     equal(result.warnings().length, 0);
 }
 
-/* Write tests here
-test('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
+//
+// .card {
+//     align-items: center;
+//     display: flex;
+//     padding: 16px;
+//     border-radius: 10px;
+// }
+//
+// .secondary,
+//     input::placeholder {
+//     color: #888;
+// }
 
-*/
+// .card {
+//     -webkit-display: flex;
+//     -moz-display: flex;
+//     display: flex;
+//     align-items: center;
+//     padding: 16px;
+//     -webkit-border-radius: 10px;
+//     -moz-border-radius: 10px;
+//     -ms-border-radius: 10px;
+//     -o-border-radius: 10px;
+//     border-radius: 10px;
+// }
+//
+// input::-ms-input-placeholder {
+//     color: #888;
+// }
+//
+// input::-moz-placeholder {
+//     color: #888;
+// }
+//
+// .secondary,
+//     input::placeholder {
+//     color: #888;
+// }
+
+test("adds vendor prefixes", async () => {
+    await run(
+        ".card { display: flex; align-items: center; padding: 16px; border-radius: 10px; } .secondary, input::placeholder { color: #888; }",
+        ".card { -webkit-display: flex; -moz-display: flex; display: flex; align-items: center; padding: 16px; -webkit-border-radius: 10px; -moz-border-radius: 10px; -ms-border-radius: 10px; -o-border-radius: 10px; border-radius: 10px; } input::-ms-input-placeholder { color: #888; } input::-moz-placeholder { color: #888; } .secondary, input::placeholder { color: #888; }",
+        {
+            vendorPrefixes: {
+                display: ["webkit", "moz"],
+                "border-radius": ["webkit", "moz", "ms", "o"],
+                "::placeholder": ["ms-input", "moz"],
+            },
+        }
+    );
+});
