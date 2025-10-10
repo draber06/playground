aecd007bddaea616ede436b00d38ca339f35f99b
Alexei Eremin <draber06@gmail.com>
2025-10-10 12:56:21 +0300

    solve yandex dfs

---
+++ y-interview-example/5.dfs.js
@@ -0,0 +1,101 @@
+import assert from "assert";
+/*
+    Дана древовидная структура следующего формата:
+
+    const tree = {
+        type: "nested",
+        children: [
+            { type: "added", value: 42 },
+            {
+                type: "nested",
+                children: [
+                    {
+                        type: "added",
+                        value: 43,
+                    },
+                ],
+            },
+            { type: "added", value: 44 },
+            ...
+        ],
+    };
+
+    Необходимо написать функцию `getNodes(tree, type)`,
+    которая возвращает все ноды в порядке следования, соответствующие переданному типу.
+
+    Глубина вложенности любая.
+ */
+
+// Time - O(n)
+// Space - O(n)
+function getNodesDfs(tree, type) {
+    const stack = [tree];
+    const result = [];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+
+        if (node.type === type) {
+            result.push(node);
+        }
+
+        const children = node.children;
+        if (children) {
+            for (let i = children.length - 1; i >= 0; i--) {
+                stack.push(children[i]);
+            }
+        }
+    }
+
+    return result;
+}
+
+function getNodesBfs(tree, type) {
+    const queue = [tree];
+    const result = [];
+    let i = 0;
+
+    while (i < queue.length) {
+        const node = queue[i++];
+        if (node.type === type) {
+            result.push(node);
+        }
+
+        if (node.children) {
+            for (const child of node.children) {
+                queue.push(child);
+            }
+        }
+    }
+
+    return result;
+}
+
+const tree = {
+    type: "nested",
+    children: [
+        { type: "added", value: 42 },
+        {
+            type: "nested",
+            children: [
+                {
+                    type: "added",
+                    value: 43,
+                },
+            ],
+        },
+        { type: "added", value: 44 },
+    ],
+};
+
+assert.deepEqual(getNodesDfs(tree, "added"), [
+    { type: "added", value: 42 },
+    { type: "added", value: 43 },
+    { type: "added", value: 44 },
+]);
+
+assert.deepEqual(getNodesBfs(tree, "added"), [
+    { type: "added", value: 42 },
+    { type: "added", value: 43 },
+    { type: "added", value: 44 },
+]);
