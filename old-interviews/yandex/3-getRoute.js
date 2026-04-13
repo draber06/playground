93eca2337d5e3f9702d08edf8f15d9076fabbd97
Alexei Eremin <draber06@gmail.com>
2026-04-13 18:34:04 +0300

    refactor(old-interviews:yandex): remove solution for get route

--- old-interviews/yandex/3-getRoute.js
+++ old-interviews/yandex/3-getRoute.js
@@ -1,31 +1,7 @@
 import assert from "assert";
 
 function getRoute(tickets = []) {
-    const destinations = new Set();
-    const routes = new Map();
-
-    for (const ticket of tickets) {
-        routes.set(ticket.from, ticket);
-
-        destinations.add(ticket.to);
-    }
-
-    let start = null;
-    for (const ticket of tickets) {
-        if (!destinations.has(ticket.from)) {
-            start = ticket.from;
-        }
-    }
-
-    const route = [];
-
-    while (routes.has(start)) {
-        const nextTicket = routes.get(start);
-        route.push(nextTicket);
-        start = nextTicket.to;
-    }
-
-    return route;
+    return [];
 }
 
 assert.deepEqual(
